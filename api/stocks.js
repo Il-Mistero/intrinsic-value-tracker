04a6c8f544260375b2198bf5879674f9fcf2d049
Lo Sviluppatore Misterioso. <126560508+Il-Mistero@users.noreply.github.com>
2025-09-01 09:07:20 +0100

    initial commit

--- api/stocks.js
+++ api/stocks.js
@@ -31,6 +31,7 @@ export default async function handler(req, res) {
       eps: quote.defaultKeyStatistics?.trailingEps || null,
       peRatio: quote.summaryDetail?.trailingPE || null,
       forwardPE: quote.summaryDetail?.forwardPE || null,
+      pegRatio: quote.defaultKeyStatistics?.pegRatio || null,
       bookValue: quote.defaultKeyStatistics?.bookValue || null,
       totalRevenue: quote.financialData?.totalRevenue || null,
       profitMargins: quote.financialData?.profitMargins || null,
@@ -45,4 +46,5 @@ export default async function handler(req, res) {
     res.status(500).json({ error: err.message });
   }
 }
+
 
